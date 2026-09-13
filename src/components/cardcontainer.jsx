af2c19a671af8d9e68e310258bfc2d0dead42903
tanvir <oxyzen2416256@gmail.com>
2026-09-13 15:52:59 +0600

    final touch

--- src/components/cardcontainer.jsx
+++ src/components/cardcontainer.jsx
@@ -2,7 +2,8 @@ import React, { use, useState } from 'react';
 import Card from './card';
 import Picker from './picker';
 
-const cardcontainer = ({dataPromise,added,setAdded}) => {
+const cardcontainer = ({dataPromise,added,setAdded}) => {\
+    //final touch 
 
     const data=use(dataPromise);
     // logical operation
